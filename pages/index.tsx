160843d4ff0c3aeb1ff6068a55fc55bf856136c2
mathesond2 <mathesond2@gmail.com>
2023-03-14 10:05:11 -0400

    add pre-commit hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -56,6 +56,25 @@ export default function Home() {
             <h2 className={inter.className}>
               Docs <span>-&gt;</span>
             </h2>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
+            <p>hello</p>
             <p className={inter.className}>Find in-depth information about Next.js features and&nbsp;API.</p>
           </a>
 
