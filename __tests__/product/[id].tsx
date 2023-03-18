e5c6402c7811be05f533c7eef2cd5efe22abdd7f
mathesond2 <mathesond2@gmail.com>
2023-03-17 23:24:28 -0400

    re-implement test dir

--- __tests__/product/[id].tsx
+++ __tests__/product/[id].tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import ProductPage from '../../product/[id]';
+import ProductPage from '../../pages/product/[id]';
 import '@testing-library/jest-dom';
 
 const product = {
